096fc48b44e581c9573dc24d8fd92a515791d046
Mateo Alvarenga <bytemat.eo@gmail.com>
2025-03-25 13:58:49 +0800

    refine dark mode

--- static/js/darkmode.js
+++ static/js/darkmode.js
@@ -1,11 +1,20 @@
-window.onload = function() {
-    if (localStorage.getItem('darkMode') === 'true') {
-        document.body.classList.add('dark-mode');
-    }
-};
 
-function toggleDarkMode() {
-    document.body.classList.toggle('dark-mode');
-    let isDarkMode = document.body.classList.contains('dark-mode');
-    localStorage.setItem('darkMode', isDarkMode);
+const themeSwitch = document.getElementById('theme-switch')
+const body = document.body;
+
+themeSwitch.addEventListener("click", () => {
+
+    body.classList.toggle("darkmode");
+
+    darkmode = localStorage.getItem('darkmode')
+    if (body.classList.contains("darkmode")) 
+        localStorage.setItem("theme", "darkmode");
+    else 
+        localStorage.setItem("theme", "lightmode");
+})
+
+if (localStorage.getItem("theme") === "darkmode") {
+    body.classList.add("darkmode");
+} else {
+    body.classList.add("lightmode")
 }
